c3450441ffb82211f3d582be1d2a06a19192b3be
LeticiaBapFelix <leticiafelix2799@gmail.com>
2024-06-10 12:56:51 -0300

    Refatorando o código

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ restaurante_praca = Restaurante('Praça', 'Gourmet')
 bebida_suco = Bebida('Suco de melancia', 5, 'Grande')
 prato_paozinho = Prato('Pãozinho', 2, 'O melhor páozinho da cidade!')
 
-restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
-restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
     print(bebida_suco)
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,5 +1,5 @@
 from modelos.avaliacao import Avaliacao
-
+from modelos.cardapio.item_cardapio import ItemCardapio
 class Restaurante:
 
     restaurantes = []
@@ -41,9 +41,6 @@ class Restaurante:
         media = round(soma / quantidade_notas, 1)
         return media
     
-    def adicionar_bebida_no_cardapio(self, bebida):
-        self._cardapio.append(bebida)
-
-    def adicionar_prato_no_cardapio(self, prato):
-        self._cardapio.append(prato)
-
+    def adicionar_no_cardapio(self, item):
+        if isinstance(item, ItemCardapio):
+            self._cardapio.append(item)
